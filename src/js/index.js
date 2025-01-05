6347eda02dbda5f12aa1a6961f278a7135ff3539
danylyoenko <lockmaster19@gmail.com>
2025-01-05 23:23:27 +0200

    cmplt hw

---
+++ src/js/index.js
@@ -0,0 +1,59 @@
+//first
+
+const myAge = 17;
+console.log(myAge);
+
+//second
+
+const myName = "Danyl";
+console.log(myName);
+
+// third
+
+const isStudent = true;
+console.log(isStudent);
+
+//four
+
+const myString =
+  "«Людину розчаровує не сам світ, а власні очікування та припущення. Все, що відбувається у світі — реально, все, що на думку людини мало статися, — нереально» — Жак Фреско";
+console.log(myString);
+
+// fifth
+
+const myNumber = 34;
+const sum = 10 + myNumber;
+
+console.log(sum);
+
+// sixth
+
+const myNull = null;
+console.log(myNull);
+
+// seventh
+
+const name = prompt("Write your name:");
+alert("Hello " + name + "!");
+
+eighth;
+
+const confirmation = confirm("Are you agree with our rules?");
+
+if (confirmation === true) {
+  alert("Thank you for confirmation!");
+} else {
+  alert("Action canceled!");
+}
+
+// ninth
+
+alert("This action are danger!");
+
+const action = confirm("Are you agree this action?");
+
+if (action === true) {
+  alert("Thank you for confirmation!");
+} else {
+  alert("Action canceled!");
+}
